4aa56db06b50a5e7556a2c5084aa5b0b1e2eb045
Tuananh28597 <86956980+Tuananh28597@users.noreply.github.com>
2021-08-05 17:19:11 +0700

    hien thi snt tu 2 den 100

--- src/hienthi100snt.java
+++ src/hienthi100snt.java
@@ -1,23 +1,15 @@
-package com.codegym;
+
 import java.util.Scanner;
-public class Main {
+public class hienthi100snt {
+
 
     public static void main(String[] args) {
-        int numbers;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Nhập n : ");
-        numbers = input.nextInt();
-        int count = 0;
-        int a = 2;
-        while (count < numbers){
-            if(check(a)){                    //goi ham check snt check() gia tri a
-                System.out.println(a + "");
-                count++;
+
+        for (int i = 2; i < 100; i++) {
+            if(check(i)){
+                System.out.println(i+"");
             }
-            a++;
         }
-
-
     }
 
     public static boolean check(int n) {   //ham check dieu kien snt, kieu boolean
@@ -33,6 +25,5 @@ public class Main {
         }
 
     }
-
 
 }
